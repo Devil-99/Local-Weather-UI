ebbc327fc99b45b2b862ed98ac5451ca91e76c15
Souvik Maji <souvikmaji1999@gmail.com>
2023-10-26 00:32:16 +0530

    new host name for server updated

--- src/utils/ApiRoutes.js
+++ src/utils/ApiRoutes.js
@@ -1,2 +1,3 @@
-export const host="http://localhost:8000";
+export const localhost="http://localhost:8000";
+export const host = "https://local-weather-server.onrender.com";
 export const locationRoute = `${host}/location`;
